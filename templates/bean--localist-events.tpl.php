5a3642ecf0c1e72ca5d2ab93cc8208189be19ac5
kevincrafts <kevin.crafts@colorado.edu>
2017-08-31 13:18:25 -0600

    adding api integrations, templates, admin forms

---
+++ templates/bean--localist-events.tpl.php
@@ -0,0 +1,14 @@
+<div class="localist-events">
+
+  <script type="text/javascript"
+src="<?php print $widget_url; ?>"></script>
+
+
+
+  <?php print $widget_url; ?>
+  <?php
+    foreach ($widget_configs as $config) {
+    //  print $config . '</br>';
+    }
+  ?>
+</div>
