26185f32f512126ccde559e401c3194fa2ccabe0
kevincrafts <kevin.crafts@colorado.edu>
2017-09-01 13:42:20 -0600

    adding caching, filter options

--- templates/bean--localist-events.tpl.php
+++ templates/bean--localist-events.tpl.php
@@ -1,14 +1,4 @@
 <div class="localist-events">
-
   <script type="text/javascript"
 src="<?php print $widget_url; ?>"></script>
-
-
-
-  <?php print $widget_url; ?>
-  <?php
-    foreach ($widget_configs as $config) {
-    //  print $config . '</br>';
-    }
-  ?>
 </div>
