ea82ce073823c057c11b7a782992ca15f6e6d130
kevincrafts <kevin.crafts@colorado.edu>
2017-09-05 11:56:53 -0600

    adding link field, removing uncecessary exclusion fields

--- templates/bean--localist-events.tpl.php
+++ templates/bean--localist-events.tpl.php
@@ -2,3 +2,11 @@
   <script type="text/javascript"
 src="<?php print $widget_url; ?>"></script>
 </div>
+<?php
+  if (!empty($content['field_localist_link'])):
+?>
+  <div class="localist-link more-link">
+    <?php print render($content['field_localist_link']); ?>
+  </div>
+<?php endif; ?>
+<?php // print $widget_url; ?>
