697c55d3e7f07a2e8000c489cf23de6bc0590eab
kevincrafts <kevin.crafts@colorado.edu>
2018-05-04 11:11:53 -0600

    Adding enable check for localist template

--- templates/bean--localist-events.tpl.php
+++ templates/bean--localist-events.tpl.php
@@ -1,12 +1,13 @@
-<div class="localist-events <?php print $localist_classes; ?>">
-  <script type="text/javascript"
-src="<?php print $widget_url; ?>"></script>
-</div>
-<?php
-  if (!empty($content['field_localist_link'])):
-?>
-  <div class="localist-link more-link">
-    <?php print render($content['field_localist_link']); ?>
+<?php if (variable_get('express_localist_enabled', TRUE): ?>
+  <div class="localist-events <?php print $localist_classes; ?>">
+    <script type="text/javascript"
+  src="<?php print $widget_url; ?>"></script>
   </div>
+  <?php
+    if (!empty($content['field_localist_link'])):
+  ?>
+    <div class="localist-link more-link">
+      <?php print render($content['field_localist_link']); ?>
+    </div>
+  <?php endif; ?>
 <?php endif; ?>
-<?php // print $widget_url; ?>
